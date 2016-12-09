cdc156988e05a89bfa5d2ac8566c05a5fceedf50
Daniel Suissa <danielsuissa@172-17-71-205.DYNAPOOL.NYU.EDU>
2016-12-09 11:37:13 -0500

    added befried, add interest, and join group

--- pythonCode.py
+++ pythonCode.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, session, url_for, redirect
 import pymysql.cursors
 import hashlib
 
+globUser = None
+
 def computeMD5hash(string):
     m = hashlib.md5()
     m.update(string.encode('utf-8'))
@@ -13,7 +15,7 @@ app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 conn = pymysql.connect(host='localhost',
                        user='root',
-                       password='',
+                       password='root',
                        db='findfolks',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
@@ -23,7 +25,7 @@ conn = pymysql.connect(host='localhost',
 @app.route('/' , methods=['GET','POST'])
 def index():
     #events of past 3 days
-    #username = session['username']
+    print(globUser)
     cursor = conn.cursor()
     query = 'SELECT * FROM an_event WHERE start_time >= cast((now()) as date) AND start_time < cast((now() + interval 29 day) as date)'
     cursor.execute(query)
@@ -31,11 +33,27 @@ def index():
     cursor.close()
     error = None
     if(nextThreeDays):
-        return render_template('index.html', posts = nextThreeDays)
+        return render_template('index.html', posts = nextThreeDays, message = globUser)
     else:
         error = "Sorry, no upcoming events"
-        return render_template('index.html', errorUpcoming = error)
-
+        return render_template('index.html', errorUpcoming = error, message = globUser)
+#home
+@app.route('/home' , methods=['GET','POST'])
+def home():
+    #events of past 3 days
+    print(globUser)
+    cursor = conn.cursor()
+    query = 'SELECT * FROM an_event WHERE start_time >= cast((now()) as date) AND start_time < cast((now() + interval 29 day) as date)'
+    cursor.execute(query)
+    nextThreeDays = cursor.fetchall()
+    cursor.close()
+    error = None
+    if(nextThreeDays):
+        return render_template('home.html', posts = nextThreeDays, message = globUser)
+    else:
+        error = "Sorry, no upcoming events"
+        return render_template('home.html', errorUpcoming = error, message = globUser)
+        
 #index execution page
 @app.route('/indexfilter'  ,  methods =['GET','POST'])
 def indexfilter():
@@ -80,7 +98,7 @@ def loginAuth():
     username = request.form['username']
     password = request.form['password']
 
-    password = computeMD5hash(password)
+    #password = computeMD5hash(password)
     cursor = conn.cursor()
     query = 'SELECT * FROM member WHERE username = %s AND password = %s'
     cursor.execute(query,(username,password))
@@ -96,6 +114,8 @@ def loginAuth():
         nextThreeDays = cursor.fetchall()
         cursor.close()
         if(nextThreeDays):
+                global globUser
+                globUser = username
                 return render_template('index.html' ,posts = nextThreeDays, message = username)
         else:
             error = "Sorry, no upcoming events"  
@@ -157,20 +177,23 @@ def registerAuth():
             conn.commit()
             cursor.close()
             return render_template('login.html')
-        else:
+        elif (interest != "" and keyword != ""):
             query = 'SELECT * FROM interest'
             cursor.execute(query)
             interest = cursor.fetchall()
             cursor.close()
             error = "Only insert the keyword directly to the right of your interest of choice as listed"
             return render_template('register.html', posts = interest, error = error)
+        else:
+            ins = 'INSERT INTO member VALUES (%s,%s,%s,%s,%s,%s)'
+            cursor.execute(ins , (username,password,firstname,lastname,email,zipcode))
+            conn.commit()
+            return render_template('login.html')
+        
+        
 
 
-#home page
-@app.route('/home' , methods=['GET', 'POST'])
-def home():
-    username = session['username']
-    return render_template('home.html', username = username)
+
 
 #USE CASE 3
 @app.route('/viewUpcomingEvents' ,  methods=['GET', 'POST'])
@@ -394,13 +417,14 @@ def rate_eventget():
         error = "You cannot rate any events at this time"
         return render_template('rate_event.html' , error = error)
 
-
+'''
 @app.route('/friends_events' , methods = ['GET', 'POST'])
 def friends_events():
     return render_template('friends_events.html')
+'''
 
-@app.route('/friend' , methods = ['GET', 'POST'])
-def friend():
+@app.route('/friends-events' , methods = ['GET', 'POST'])
+def friendsEvents():
     username = session['username']
     cursor = conn.cursor() 
     query = 'SELECT friend_to,an_event.event_id,title,description,start_time,end_time,location_name,zipcode FROM friend, sign_up, an_event WHERE friend.friend_to = sign_up.username AND sign_up.event_id = an_event.event_id AND friend_of = %s'
@@ -408,44 +432,112 @@ def friend():
     data = cursor.fetchall()
     error = None
     if(data):
-        return render_template('friend.html' , message = username , posts = data)
+        return render_template('signup.html' , message = username , posts = data)
     else:
-        return render_template('friend.html' , message = username)
+        return render_template('signup.html' , message = username)
 
+@app.route('/friend' , methods = ['GET', 'POST'])
+def friend():
+    username = session['username']
+    cursor = conn.cursor() 
+    query = 'SELECT * from friend, member WHERE friend_of = username AND friend_to = %s '
+    cursor.execute(query , (username))
+    data = cursor.fetchall()
+    if(data):
+        return render_template('friend.html' , message = username, posts = data)
+    else:
+        return render_template('friend.html' , message = username, error = "You have no friends at the moment")
 @app.route('/friendAuth' , methods = ['GET', 'POST'])
 def friendAuth():
     username = session['username']
     friend_username = request.form['username']
     cursor = conn.cursor()
-    query = 'SELECT friend_to from friend WHERE friend_of = %s'
+    query = 'SELECT friend_of from friend WHERE friend_to = %s AND friend_of = %s'
+    cursor.execute(query , (friend_username,username))
+    exists = cursor.fetchone()
+    query = 'SELECT * from friend, member WHERE friend_of = username AND friend_to = %s '
     cursor.execute(query , (username))
-    all_friends = fetchall()
-
+    data = cursor.fetchall()
     query = 'SELECT username from friend WHERE friend_of = %s'
-    cursor.execute(query , (friend_username))
-    exist_friend = fetchone()
-    '''
-    if(!all_friends):
-        all_friends = None
-    if(exist_friend):
-        return render_template('friend.html', posts = all_friends, error = friend_username + " is already your friend!")
+    if(exists):
+        render_template('friend.html',posts = data, error = "This person is in your friends list already!" )
     else:
-        query = 'SELECT username from member WHERE username = %s'
+        query = 'SELECT * from member WHERE username = %s'
         cursor.execute(query , (friend_username))
-        exist_member = fetchone()
-        if(exist_member):
-            query = 'INSERT INTO friend VALUES (%s, %s)'
-            cursor.execute(query , (username, friend_username))
-            cursor.commit()
-            return render_template('friend.html', posts = all_friends, error = friend_username + " is now your friend!")
+        exists_member = cursor.fetchone()
+        if(exits_member):
+            render_template('friend.html',posts = data, error = "Successfully added friend!" )
         else:
-            return render_template('friend.html', posts = all_friends, error = "Could not find " + friend_username + " on FindFolks")
-            '''
+            render_template('friend.html',posts = data, error = "There is no such member in findFolks" )
+
+#Define route for join group
+@app.route('/joinGroup' , methods=['GET', 'POST'])
+def joinGroup():
+        cursor = conn.cursor()
+        query = 'SELECT * FROM a_group'
+        cursor.execute(query)
+        groups = cursor.fetchall()
+        cursor.close()
+        if(groups):
+            return render_template('join-group.html' , posts = groups)
+        else:
+            return render_template('join-group.html' , error = "No groups in FindFolks")
+#join group 
+@app.route('/joinGroupExec' , methods=['GET', 'POST'])
+def joinGroupExec():
+    #grab information from joing-group page
+    group_id = request.form['group_id']
+    cursor = conn.cursor()
+    query = 'SELECT * FROM a_group WHERE group_id = %s'
+    cursor.execute(query,(group_id))
+    data = cursor.fetchone()
+    error = None
+    #default table
+    query = 'SELECT * FROM a_group'
+    cursor.execute(query)
+    groups = cursor.fetchall()
+    cursor.close()
+    if(data):
+        error = "You successfully joined the group!"
+        return render_template('join-group.html', posts = groups ,error = error)
+    else:
+        error = "This group Id does not exist, try another one!"
+        return render_template('join-group.html', posts = groups ,error = error)
+
+@app.route('/add-interest' , methods=['GET', 'POST'])
+def addInterest():
+    return render_template('add-interest.html' )
+    
+@app.route('/insert-interest' , methods=['GET', 'POST'])
+def insertInterest():
+    username = session['username']
+    #username = session['username']
+    interest = request.form['category']
+    keyword = request.form['keyword'] 
+    cursor = conn.cursor()
+    query = "SELECT * FROM interested_in WHERE category = %s AND keyword = %s AND username = %s"
+    cursor.execute(query,(interest,keyword, username))
+    exists = cursor.fetchone()
+    if(exists):
+        cursor.close()
+        return render_template('add-interest.html', error="you are already interested in that!" )
+    else:
+        ins = 'INSERT INTO interest (category,keyword) VALUES (%s,%s)'
+        cursor.execute(ins , (interest,keyword))
+        conn.commit()
+        ins = 'INSERT INTO interested_in (username,category,keyword) VALUES (%s,%s,%s)'
+        cursor.execute(ins , (username,interest,keyword))
+        conn.commit()
+        cursor.close()
+        return render_template('add-interest.html', error="successfully added interest" )
+        
+
 @app.route('/logout')
 def logout():
-	session.pop('username')
-
-	return redirect('/')
+    global globUser
+    globUser = None
+    session.pop('username')
+    return redirect('/')
 
 
 if __name__ == "__main__":
